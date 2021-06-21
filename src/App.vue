315871a707a2a51ef361fdf14998bb35d104e32b
Louis MOUHAT <louis.mouhat@gmail.com>
2021-06-22 00:52:26 +0200

    add basic vue + vuex + router + material project

---
+++ src/App.vue
@@ -0,0 +1,65 @@
+<template>
+  <div class="page-container">
+    <md-app md-mode="reveal">
+      <md-app-toolbar class="md-primary">
+        <md-button class="md-icon-button" @click="menuVisible = !menuVisible">
+          <md-icon>menu</md-icon>
+        </md-button>
+        <span class="md-title">My Title</span>
+      </md-app-toolbar>
+
+      <md-app-drawer :md-active.sync="menuVisible">
+        <md-toolbar class="md-transparent" md-elevation="0">
+          Navigation
+        </md-toolbar>
+
+        <md-list>
+          <md-list-item to="/">
+            <md-icon>home</md-icon>
+            <span class="md-list-item-text">Home</span>
+          </md-list-item>
+
+          <md-list-item to="/about">
+            <md-icon>help</md-icon>
+            <span class="md-list-item-text">About</span>
+          </md-list-item>
+
+          <md-list-item>
+            <md-icon>help</md-icon>
+            <span class="md-list-item-text">Inbox</span>
+          </md-list-item>
+        </md-list>
+      </md-app-drawer>
+
+      <md-app-content>
+        <router-view/>
+      </md-app-content>
+    </md-app>
+  </div>
+</template>
+
+<script>
+export default {
+  data: () => ({
+    menuVisible: false,
+  }),
+};
+</script>
+
+<style lang="scss">
+#app {
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+}
+
+.page-container {
+  width: 100vw;
+  height: 100vh;
+
+  .md-app {
+    width: 100%;
+    height: 100%;
+  }
+
+}
+</style>
