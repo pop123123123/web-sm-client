b4e8e5909ce91352c6e555c6f89f2c61f7b4643f
Clement Thiebault <caimezae@gmail.com>
2021-06-22 17:58:09 +0200

    First add of buttons and cards

--- src/App.vue
+++ src/App.vue
@@ -1,35 +1,6 @@
 <template>
   <div class="page-container">
     <md-app md-mode="reveal">
-      <md-app-toolbar class="md-primary">
-        <md-button class="md-icon-button" @click="menuVisible = !menuVisible">
-          <md-icon>menu</md-icon>
-        </md-button>
-        <span class="md-title">My Title</span>
-      </md-app-toolbar>
-
-      <md-app-drawer :md-active.sync="menuVisible">
-        <md-toolbar class="md-transparent" md-elevation="0">
-          Navigation
-        </md-toolbar>
-
-        <md-list>
-          <md-list-item to="/">
-            <md-icon>home</md-icon>
-            <span class="md-list-item-text">Home</span>
-          </md-list-item>
-
-          <md-list-item to="/about">
-            <md-icon>help</md-icon>
-            <span class="md-list-item-text">About</span>
-          </md-list-item>
-
-          <md-list-item>
-            <md-icon>help</md-icon>
-            <span class="md-list-item-text">Inbox</span>
-          </md-list-item>
-        </md-list>
-      </md-app-drawer>
 
       <md-app-content>
         <router-view/>
