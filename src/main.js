07b6318cd691e5b945e7bd25a8da850405bd57c8
Louis MOUHAT <louis.mouhat@gmail.com>
2021-06-28 15:12:43 +0200

    add vuetify

--- src/main.js
+++ src/main.js
@@ -6,6 +6,9 @@ import router from './router';
 import store from './store';
 import 'vue-material/dist/vue-material.min.css';
 import 'vue-material/dist/theme/default.css';
+import vuetify from './plugins/vuetify';
+import 'roboto-fontface/css/roboto/roboto-fontface.css';
+import 'material-design-icons-iconfont/dist/material-design-icons.css';
 
 Vue.use(VueMaterial);// TODO import one by one
 
@@ -14,5 +17,6 @@ Vue.config.productionTip = false;
 new Vue({
   router,
   store,
+  vuetify,
   render: (h) => h(App),
 }).$mount('#app');
