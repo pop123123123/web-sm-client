a03fad9a7fcfffd7b7bf27276b13faa724983748
Clement Thiebault <caimezae@gmail.com>
2021-07-19 14:17:01 +0200

    add error message

--- src/socket.js
+++ src/socket.js
@@ -1,12 +1,14 @@
+import action from '@/store/action-types';
+
 const socket = new WebSocket(`ws://${window.location.host}/ws/`);
 
 const plugin = () => (store) => {
   socket.onmessage = (event) => {
     const data = JSON.parse(event.data);
     console.debug('WebSocket message received:', data);
-    if (data.Err) throw data.Err;
+    if (data.Err) { store.dispatch(action.CHANGE_SOCKET_ERROR, data.Err); }
     const [mutation, argument] = Object.entries(data)[0];
-    if (mutation !== 'Ok') { store.commit(mutation, argument); }
+    if (mutation !== 'Ok' && mutation !== 'Err') { store.commit(mutation, argument); }
   };
 };
 
