a862c28d9b6dd9374bd5415859c5ec564222e8b2
Antoine Brûlé <antoine.brule@netc.eu>
2021-07-22 23:04:44 +0200

    add linter rules for webpack alias support

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,3 +1,5 @@
+const path = require('path');
+
 module.exports = {
   root: true,
   env: {
@@ -14,5 +16,14 @@ module.exports = {
     'no-console': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
     'no-debugger': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
     'vue/valid-v-slot': 'off',
+  },
+  settings: {
+    'import/resolver': {
+      alias: {
+        map: [
+          ['@', path.resolve(__dirname, './src')],
+        ],
+      },
+    },
   },
 };
