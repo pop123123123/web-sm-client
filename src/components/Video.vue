4139a1b5e7cc9acd64552a76ef6fa1a9514aa011
Louis MOUHAT <louis.mouhat@gmail.com>
2021-08-04 14:37:51 +0200

    workaround for multiple review delay

--- src/components/Video.vue
+++ src/components/Video.vue
@@ -1,7 +1,11 @@
 <template>
   <div>
-    <video ref="video" @ended="next">
-      <source :src="src" type="video/webm">
+    <video ref="video0" v-show="currentIndex % 2 === 0" @canplaythrough="loaded(0)">
+      <source :src="src[0]" type="video/webm">
+      Sorry, your browser doesn't support embedded videos.
+    </video>
+    <video ref="video1" v-show="currentIndex % 2 === 1">
+      <source :src="src[1]" type="video/webm">
       Sorry, your browser doesn't support embedded videos.
     </video>
   </div>
@@ -12,22 +16,38 @@ export default {
   name: 'Video',
   data() {
     return {
-      src: '',
+      src: ['', ''],
       currentIndex: null,
       urls: null,
+      timeout: null,
     };
   },
   methods: {
-    preview(url) {
-      const videoElem = this.$refs.video;
-      this.src = url;
-
-      // start video
+    reset() {
+      this.$set(this.src, 0, '');
+      this.$set(this.src, 1, '');
+      this.urls = null;
+      this.currentIndex = null;
+      if (this.timeout) clearTimeout(this.timeout);
+      this.timeout = null;
+    },
+    play(element) {
+      this.$refs[`video${element}`].play();
+    },
+    load(url, element) {
+      const videoElem = this.$refs[`video${element}`];
+      videoElem.pause();
+      this.$set(this.src, element, url);
       videoElem.load();
       videoElem.currentTime = 0;
-      videoElem.play();
+      return videoElem;
+    },
+    preview(url, element) {
+      // load and start video
+      this.load(url, element).play();
     },
     startPreview() {
+      this.reset();
       // requested preview
       if (!this.toPreview) return;
       if (this.toPreview.find((_, i) => this.$store.getters.getPreview(i) === undefined)) return;
@@ -35,16 +55,33 @@ export default {
       this.urls = this.toPreview.map((_, i) => window.URL.createObjectURL(this.$store.getters
         .getPreview(i)));
 
-      this.$refs.video.pause();
-      this.preview(this.urls[0]);
       this.currentIndex = 0;
+      this.load(this.urls[0], 0);
+    },
+    loaded(element) {
+      if (this.currentIndex === 0) {
+        const el = this.$refs[`video${element}`];
+        if (this.urls.length > 1) {
+          this.load(this.urls[1], 1);
+          this.timeout = setTimeout(
+            this.next,
+            el.duration * 1000,
+          );
+        }
+        el.play();
+      }
     },
     next() {
-      console.log('next');
       this.currentIndex += 1;
-      if (this.toPreview.length > this.currentIndex) {
-        this.preview(this.urls[this.currentIndex]);
+      if (this.toPreview.length > this.currentIndex + 1) {
+        this.load(this.urls[this.currentIndex + 1], (this.currentIndex + 1) % 2);
+        const el = this.$refs[`video${this.currentIndex % 2}`];
+        this.timeout = setTimeout(
+          this.next,
+          el.duration * 1000,
+        );
       }
+      this.play(this.currentIndex % 2);
     },
   },
   watch: {
@@ -58,7 +95,6 @@ export default {
     },
   },
   mounted() {
-    // this.$refs.video.addEventListener('ended', this.next);
     this.startPreview();
     this.$store.state.videoComponent = this;
   },
