84f9c4f26b3c804cff6793342f17ad0c640539a4
Antoine Brûlé <antoine.brule@netc.eu>
2021-08-06 17:01:55 +0200

    add debug module

--- src/main.js
+++ src/main.js
@@ -1,11 +1,16 @@
 import Vue from 'vue';
 
+import Debug from 'debug';
 import App from './App.vue';
 import router from './router';
 import store from './store';
 import vuetify from './plugins/vuetify';
 import 'roboto-fontface/css/roboto/roboto-fontface.css';
 import 'material-design-icons-iconfont/dist/material-design-icons.css';
+
+if (process.env.NODE_ENV === 'development') {
+  Debug.enable('app*');
+}
 
 Vue.config.productionTip = false;
 
